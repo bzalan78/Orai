bd7281ee194f282dab7da68cb69add14ed89e9de
bzalan78 <bokanyizalan@turr.hu>
2023-12-12 08:45:21 +0100

    szepites

--- fizika.py
+++ fizika.py
@@ -5,15 +5,15 @@ j=3
 if k==l:
     m=int(input("Kérem méterben a megtett távolságot. "))
     v=int(input("Kérem a sebességet m/s-ban. "))
-    print("idő=",m//v)
+    print("idő =",m//v,"mp")
 elif k==h:
     m=int(input("Kérem méterben a megtett távolságot. "))
     t=int(input("Kérem az időt mp-ben. ")) 
-    print("Sebesség=",m//t)
+    print("Sebesség =",m//t,"m/s")
 elif k==j:
     v=int(input("Kérem a sebességet m/s-ban. "))
     t=int(input("Kérem az időt mp-ben. ")) 
-    print("A megtett út=",v*t)
+    print("A megtett út =",v*t,"m")
 
 
     
