84f489a53086006ab40e489bc3862cf750b2e7a0
bzalan78 <bokanyizalan@turr.hu>
2023-12-12 08:29:25 +0100

    3.if

--- fizika.py
+++ fizika.py
@@ -1,8 +1,12 @@
 k=input("Az idő (1),a sebesség (2) vagy a megtett út (3) hiányzik?")
 if k==1:
-    int(input("Kérem méterben a megtett távolságot."))
-    int(input("Kérem a sebességet m/s-ban."))
+    m=int(input("Kérem méterben a megtett távolságot."))
+    v=int(input("Kérem a sebességet m/s-ban."))
 elif k==2:
-    int(input("Kérem méterben a megtett távolságot."))
-    int(input("Kérem az időt mp-ben")) 
+    m=int(input("Kérem méterben a megtett távolságot."))
+    t=int(input("Kérem az időt mp-ben")) 
+elif k==3:
+    v=int(input("Kérem a sebességet m/s-ban."))
+    t=int(input("Kérem az időt mp-ben")) 
+
     
