bb528a23e22b9097654366d3901448b82f7905c8
bzalan78 <bokanyizalan@turr.hu>
2023-12-12 08:35:01 +0100

    print2

--- fizika.py
+++ fizika.py
@@ -6,6 +6,7 @@ if k==1:
 elif k==2:
     m=int(input("Kérem méterben a megtett távolságot."))
     t=int(input("Kérem az időt mp-ben")) 
+    print("Sebesség=",m//t)
 elif k==3:
     v=int(input("Kérem a sebességet m/s-ban."))
     t=int(input("Kérem az időt mp-ben")) 
